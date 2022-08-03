ae78d729df5f89f968b8e30b9a69914b6bbcee2a
Refusado <orefusado@gmail.com>
2022-08-03 17:22:29 -0300

    método para criar usuários

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
@@ -8,6 +8,12 @@ import { User } from '../models/user';
 })
 export class UserService {
   apiUrl = 'https://sheet.best/api/sheets/01ae3d2c-cd5f-4d33-97bb-e5193331906c'
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      // 'token': '21392131',
+    })
+  }
 
   constructor(private httpClient: HttpClient) { }
 
@@ -15,5 +21,10 @@ export class UserService {
   // RETORNA A LISTA DE USUÁRIOS
   getUsers(): Observable<User[]> {
     return this.httpClient.get<User[]>(this.apiUrl);
+  }
+
+  // SALVA USUARIO NO BANCO
+  postUser(user: User): Observable<User> {
+    return this.httpClient.post<User>(this.apiUrl, user, this.httpOptions);
   }
 }
