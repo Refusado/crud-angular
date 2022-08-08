818bda5632c897d7641eca260cbe39fdecc5c2b3
Refusado <orefusado@gmail.com>
2022-08-08 01:24:50 -0300

    melhorias e salvando as tarefas no localstorage

--- src/app/pages/todo/todo-list/todo-list.component.ts
+++ src/app/pages/todo/todo-list/todo-list.component.ts
@@ -7,33 +7,27 @@ import { ToDo } from 'src/app/models/to-do';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
-  todos: ToDo[] = [
-    {
-      id: 1,
-      title: 'comer uns cus',
-      done: false,
-    },
-    {
-      id: 1,
-      title: 'comer uns cus',
-      done: false,
-    },
-  ];
+  todos: ToDo[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    let localTodos: string | null = localStorage.getItem('todos');
+    if (localTodos)
+      this.todos = JSON.parse(localTodos);
   }
 
   addToDo(title: string): void {
     const id = this.todos.length + 1;
     this.todos.push(new ToDo(id, title, false));
 
-    console.log(this.todos);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 
   removeTodo(todo: any) {
     let i = this.todos.indexOf(todo);
     this.todos.splice(i, 1);
+
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
